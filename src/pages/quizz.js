e2c30b09900ec4aeef2bac92b9285f1ae5d0104d
Necakco <khoavencyti@gmail.com>
2023-06-14 20:49:19 +0700

    fn

--- src/pages/quizz.js
+++ src/pages/quizz.js
@@ -1,41 +1,67 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className="heroBanner">
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            idk
-          </Link>
-        </div>
+        <center>
+          <img className="logonho" src="./img/logo.png"></img>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--ifm-color-primary-light"
+              to="/docs/intro"
+            >
+              Start Learning NOW 😉
+            </Link>
+          </div>
+          <br></br>
+          <p>Im better </p>
+        </center>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      description="Description will go into a meta tag in <head />"
+    >
+      <div className="idkquizz">
+        <HomepageHeader />
+        <main>
+          <div className="mainquizz">
+            <center>
+              <div className="flex-container">
+                <div className="conterse">
+                  <h1>Tự luyện</h1>
+                  <a className="button button--secondary button--ifm-color-primary-light">
+                    Start
+                  </a>
+                </div>
+                <div className="conterse">
+                  <h1>Thi online</h1>
+                  <a className="button button--secondary button--ifm-color-primary-light">
+                    Start
+                  </a>
+                </div>
+              </div>
+            </center>
+          </div>
+        </main>
+      </div>
     </Layout>
   );
 }
